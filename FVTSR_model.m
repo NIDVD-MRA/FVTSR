8fa1d43ee753e57e57e9dd42a579b24b39dc6528
yangjiayuyangjiayu <32620230156360@stu.xmu.edu.cn>
2024-11-05 12:24:12 +0800

    Update FVTSR_model.m

--- FVTSR_model.m
+++ FVTSR_model.m
@@ -1,13 +1,8 @@
-%%
-%相较于10_15_1版本，Ct =（比值等式解 + 函数与x轴交点）* 倍率（经验值），倍率改变为 （0.4 * 比值等式解 + 0.6 * 函数与x轴交点 ）
 clear all;
 clc;
 
-
-P_overall = xlsread('E:\文献汇总\荧光检测系统\ct值算法论文\2023年Ct值算法论文\初稿\NAR稿\NAR投稿\论文用图-NAR用图\重复性数据\baseline标注--用于Ct值计算\2024-8-14 FAM 伯乐数据Ct值计算.xlsx',1,'E2:AY63');
-%P = xlsread('D:\Desktop\research on optical detection method\ct_20220505测试数据.xlsx',1,'D70:AV70');
+P_overall = xlsread('E:\***.xlsx',sheet *,'E2:AY63');
 x = 1:47;
-% P = P';
 
 s_column = size(P_overall,1);
 %s_row = size(P_overall,2);
@@ -29,16 +24,14 @@ for s_size = 1:s_column;
     plot(P);
 
     %%
-    %平滑
-    %P_smooth = P;
+    %smooth
     P_smooth = smooth(P);
     P_smooth = P_smooth';
-    % P_smooth = smoothdata(P);
     figure(2)
     plot(P_smooth);
     
     %%
-    %归一化
+    % Amplitude normalization
     P_min = min(P_smooth);
     P_max = max(P_smooth);
     P_decrease = P_smooth - P_min;
@@ -47,8 +40,7 @@ for s_size = 1:s_column;
     
 
    %%
-   %阴阳性判定（动态微分法）
-   
+   % Dynamic Differential Determination
    P_normalisation_min = min(P_normalisation);
    P_normalisation_base_min = P_normalisation - P_normalisation_min;
    P_normalisation_max = max(P_normalisation_base_min);
@@ -58,7 +50,7 @@ for s_size = 1:s_column;
    for x_judgement =1:44
        Dynamic_P_normalisation_single = P_normalisation_double(x_judgement + 1) / P_normalisation_double(x_judgement) - 1;
        Dynamic_P_normalisation(x_judgement) = Dynamic_P_normalisation_single;
-   end                                      %求动态微分值
+   end                                      
    figure(44)
    hold on;
    plot(Dynamic_P_normalisation);
@@ -73,36 +65,35 @@ for s_size = 1:s_column;
                   
 
     %%
-    % 计算一阶差分
+    % Calculate the first order difference
     d_Dynamic_P_normalisation = diff(Dynamic_P_normalisation);
     
-%     max_Dynamic_P = max(Dynamic_P);
     [max_Dynamic_P_normalisation,max_Dynamic_P_normalisation_Index] = max(Dynamic_P_normalisation);
 
-    % 初始化极大值计数
+    % Initialise the count of extremely large values
     countMax = 0;
-    % 初始化曲线后段数据统计值
+    % Initialise the statistical values of the back-end data of the curve
     count_dynamic_P_subset = 0;
 
-    startIdx = 40;    % 起始下标
-    endIdx = 44;      % 结束下标
+    startIdx = 40;    % starting subscript
+    endIdx = 44;      % end subscript
     
     
-    % 选择向量的子集--归一化P一阶微分的子集
+    % Selection of subsets of vectors - subsets of normalised P first order differentials
     subset = Dynamic_P_normalisation(startIdx:endIdx);
-    % 检查子集中是否所有值都大于阈值（曲线后期的数值是否不断上升）
+    %Check that all values in the subset are greater than the threshold 
     allGreaterThanThreshold = all(subset > 0);
     
     
-    % 遍历差分向量，寻找极大值
+    % Iterate over the difference vectors to find extreme values
     for i = 3:length(d_Dynamic_P_normalisation)-1
-       % 判断：归一化P一阶微分的一阶差分在第(i-1)个>0 && 
-       % 归一化P一阶微分的一阶差分在第i个>0 &&
-       % 归一化P一阶微分的一阶差分在第i+1个<0 &&
-       % i+1是否为归一化P一阶微分的最大值 && 
-       % 归一化P的40-45均值 > 归一化P的1-5均值
+       % Judgement: the first-order difference of the normalised P first-order differential is at the (i-1)th > 0 && 
+       % The first-order difference of the normalised P first-order differential at i > 0 &&
+       % The first-order difference of the normalised P first-order differential at the i+1st < 0 &&
+       % Whether i+1 is the maximum value of the normalised P first order differentiation && 
+       % 40-45 mean of normalised P > 1-5 mean of normalised P ?
         if d_Dynamic_P_normalisation(i-1) > 0 && d_Dynamic_P_normalisation(i) > 0 && d_Dynamic_P_normalisation(i+1) < 0 && i+1 == max_Dynamic_P_normalisation_Index && mean(P_normalisation(40:45)) > mean(P_normalisation(1:5))
-            % 如果找到一个真正的极大值
+            % If we find a truly extreme value
             countMax = countMax + 1;
         end
     end
@@ -114,33 +105,23 @@ for s_size = 1:s_column;
     end
     
     if countMax == 1;
-%        disp('阳性');
+%        disp('positive');
          else
          countMax = 0;
          v(s_size,1) = countMax;
-         disp('阴性');
+         disp('negative');
          continue
     end
          
     
     %%
-    %基线平移
-%     if mark2 == 0
-%     if zengyi < 5000  &&  mark2 == 0
+    % Background value processing
         x_Linear = base_start : base_end ;
         P_Linear = (P_smooth(base_start:base_end));
         Linear_coefficient = polyfit(x_Linear,P_Linear,1);
         Linear = Linear_coefficient(1) * x + Linear_coefficient(2) - 1;
         P_linear = P_smooth - Linear;
 
-        % for i=1:45
-        %     if P_linear(i) < 0
-        %         u = i;
-        %         for i = 1:u
-        %             P_linear(i) = 0;
-        %         end
-        %     end
-        % end
         figure(11)
         plot(Linear);
         figure(3)
@@ -150,26 +131,24 @@ for s_size = 1:s_column;
 
         
     %%
-    %二次平滑(不一定需要，目前还在用）
-%     P2 = smooth(P_linear);
+    % smooth
      P2 = P_linear;
 
     figure(4)
     plot(P2);
 
-    % P2 = P2';
 
     %%
-    %能量迁移定量模型
-    R_0 = 5; % F?ster 距离，单位为纳米
+    % Quantitative algorithms for energy migration
+    R_0 = 5; % Distance in nanometres
     inte_a = 0.5 * R_0;
     inte_b = 1.09 * R_0;
     inte_c = R_0;
     
     Gain_midpoint = 0;
-    Gain_midpoint = P2(45)*(1/2);        %增益中点
+    Gain_midpoint = P2(45)*(1/2);        % Midpoint of gain
     
-    %增益中点对应x值（左）
+    % Gain midpoint corresponds to x-value (left)
      for x = 12:45
             cs_D(x) = P2(x) - Gain_midpoint;
             if cs_D(x) <= 0
@@ -177,52 +156,48 @@ for s_size = 1:s_column;
             end
      end                                        
     
-     %增益中点对应x值（右）
+     %Gain midpoint corresponds to x-value (right)
      Gain_midpoint_right =  Gain_midpoint_left +1;
      
-     % 增益中点大于等于44，判为阴性
+     % A gain midpoint greater than or equal to 44 is considered negative.
      if Gain_midpoint_left >= 44
          countMax = 0;
          v(s_size,1) = countMax;
-         disp('阴性');
+         disp('negative');
          continue
      end
 
      
 %     cs_D = cs_D.';
-    cs_x_1 = [Gain_midpoint_left-1;Gain_midpoint_left;Gain_midpoint_right;Gain_midpoint_right+1]';        %拟合范围
+    cs_x_1 = [Gain_midpoint_left-1;Gain_midpoint_left;Gain_midpoint_right;Gain_midpoint_right+1]';        %Fitting range
     cs_y_1 = P2(Gain_midpoint_left-1:Gain_midpoint_right+1);
     cs_coefficient1 = polyfit(cs_x_1,cs_y_1,1);
-    cs_x_11 = (Gain_midpoint_right-40:1:Gain_midpoint_right+30);                               %拟合函数后取值范围（x值）
+    cs_x_11 = (Gain_midpoint_right-40:1:Gain_midpoint_right+30);                               %Range of values after fitting the function (x-values)
     % cs_x_11 = [cs_jz_z-4;cs_jz_z-3;cs_jz_z-2;cs_jz_z-1;cs_jz_z;cs_jz_y;cs_jz_y+1];     
-    cs_y_11 = cs_coefficient1(1)*cs_x_11 + cs_coefficient1(2);                                %拟合函数后取值范围内拟合值求取
+    cs_y_11 = cs_coefficient1(1)*cs_x_11 + cs_coefficient1(2);                                % After fitting the function, take the value of the range of the fitted value to find
     
-    cs_45 = cs_y_11(45);                                                                %拟合函数后45循环拟合值求取
+    cs_45 = cs_y_11(45);                                                                % After fitting the function, the 45-loop fit values are derived
    
     intersection_cs_y_11 = (0 - cs_coefficient1(2)) / cs_coefficient1(1);
      
-    
-    
-%    floor_base_end = floor(base_end);
  
      syms xx
-    % 计算FRET近似梯形比值: S1/S2（大面积（左）/小面积（右））
+    % Calculation of FRET approximate trapezoidal ratio: S1/S2 (large area (left)/small area (right))
     area_E_specific = (0.5*(inte_c - inte_a)*((R_0.^6)/(R_0.^6 + inte_c.^6) + (R_0.^6)/(R_0.^6 + inte_a.^6)))/(0.5*(inte_b - inte_c)*((R_0.^6)/(R_0.^6 + inte_b.^6) + (R_0.^6)/(R_0.^6 + inte_c.^6)));
-    % 计算荧光曲线近似三角形比值: 
+    % Calculation of the approximate triangular ratio of fluorescence curves: 
       area_fluorescence = 0.5*(P2(Gain_midpoint_left) + cs_45) * (45 - Gain_midpoint_left) / (0.5 * (Gain_midpoint_left - xx) * (P2(Gain_midpoint_left)));
-    % 求解等式未知数xx
+    % Solve the equation for the unknown xx
     equation = area_fluorescence == area_E_specific;
     solution = solve(equation, xx);
-    solution_decimal = vpa(solution, 6);%保留4位小数
-    Ct = 0.4 * solution_decimal + 0.6 * intersection_cs_y_11;
-    disp('Ct=');
+    solution_decimal = vpa(solution, 6);% Retain 4 decimal places
+    Key point = 0.4 * solution_decimal + 0.6 * intersection_cs_y_11;
+    disp('Key point =');
     disp(solution_decimal);
      
 v(s_size,1) = countMax;
-v(s_size,2) = Ct;
+v(s_size,2) = Key point;
 end
 
-%xlswrite('E:\核酸现场快速检测仪器研发\循环阈值计算方法\算法\算法优化草稿\202309优化\Ct值计算-20230902-HSL-1.xlsx',v,'sheet1','E2');
 
 
 
